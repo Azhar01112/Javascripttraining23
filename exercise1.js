106108cf9db97f219ab2929d30e3018f542c8ac9
azhar01112 <ajrusss786@gmail.com>
2023-02-06 12:05:23 +0530

    added 2 new files

---
+++ exercise1.js
@@ -0,0 +1,123 @@
+/*let english = Number(prompt("what is your english marks"));
+let hindi = Number(prompt("what is your hindi marks"));
+let math = Number(prompt("what is your math mark"));
+let science = Number(prompt("what is your science mark"));
+let sst = Number(prompt("what is your sst mark"));
+let totalmarks = english + hindi + math + science + sst;
+console.log(totalmarks);
+let percentage = (totalmarks / 500) * 100;
+console.log(percentage);*/
+
+/// let myname = "azhar";
+// let a = 20;
+// console.log(myname + a);
+// console.log(30 + "wecode");
+// console.log("myname" + "true");/*
+
+// let ans = prompt("do you drink coffee or tea");
+// if (ans === "coffee") {
+//   console.log("tea");
+// } else if (ans === "tea") {
+//   console.log("coffee");
+// }
+
+// let tem = 20;
+// if (tem < 10) {
+//   console.log("very cold");
+// } else if (tem < 30 >= 10) {
+//   console.log("cold");
+// } else if (30 <= 40);
+// {
+//   console.log("hot");
+// } else (tem > 40) {
+// console.log("very hot");
+// }
+
+// let age1 = prompt("what is your age");
+// let name = prompt("what is your name ");
+// let aadharnumber = prompt("what is your aadhar number");
+// let address = prompt("what is your address");
+// let mobilenumber = prompt("what is your mobile number");
+// if (age1 > 18) {
+//   console.log("do you want to open acount?");
+//   console.log(name, aadharnumber, address, mobilenumber);
+// } else if (age1 < 18) {
+//   console.log(" sorry please visit again");
+// }
+
+// let uu = 10;
+// let yy = 20;
+// let ans = prompt("what do you want to do");
+// if ("+") {
+//   console.log(uu + yy);
+// } else "-";
+// {
+//   console.log(uu - yy);
+// }
+// let rr = 6;
+// let qq = 4;
+// console.log(rr + qq, rr - qq, rr * qq, rr / qq);
+
+// let num11 = 26;
+// num11 % 2 === 0 ? console.log("even") : console.log("odd");
+
+// let name = prompt("what is your name ");
+// alert(name);
+
+let num8 = 30;
+let num9 = 20;
+let myname = "aaaaa";
+let mynumber = 7676888888;
+let myage = 22;
+let fathernumber = 989888866;
+let fathername = "zzzzzz";
+if (num8 < num9) {
+  console.log(myname, mynumber, myage);
+} else if (num8 > num9) {
+  console.log(fathername, fathernumber);
+}
+
+let num5 = 35;
+let num6 = 40;
+let ans = (num5 % 5 === 0 && num5 % 10) || (num6 % 5 && num6 % 10);
+console.log(num5 % 5 === 0 && num5 % 10) || (num6 % 5 && num6 % 10);
+num5 % 2 === 0 ? console.log("even") : console.log("odd");
+num6 % 2 === 0 ? console.log("even") : console.log("odd");
+
+// console.log("hello");
+// alert("hello");
+// window.alert("hii ");
+// document.write("we code");
+
+// let ans2 = confirm("do you want play ");
+// if (ans2) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+
+let num7 = 0;
+console.log(++num7);
+console.log(++num7);
+console.log(++num7);
+console.log(++num7);
+console.log(++num7);
+console.log(++num7);
+console.log(++num7);
+console.log(++num7);
+console.log(++num7);
+console.log(++num7);
+
+let num10 = 11;
+console.log(
+  --num10,
+  --num10,
+  --num10,
+  --num10,
+  --num10,
+  --num10,
+  --num10,
+  --num10,
+  --num10,
+  --num10
+);
---
+++ exercise2.js
@@ -0,0 +1,10 @@
+alert("hello");
+
+let rr = 6;
+let qq = 4;
+console.log(rr + qq, rr - qq, rr * qq, rr / qq);
+
+let num = 25;
+num % 2 === 0 ? console.log("even") : console.log("odd");
+
+alert("hello");
